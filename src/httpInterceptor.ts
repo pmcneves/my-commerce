28f09c06dc167afb94ac7304b7c80684e2ed7e82
Pedro Neves <pneves355@gmail.com>
2021-09-27 17:22:28 +0100

    setting up interceptor v2

--- src/httpInterceptor.ts
+++ src/httpInterceptor.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default () =>
+export default (): number =>
   axios.interceptors.request.use(
-    (request) => {
+    (request: AxiosRequestConfig) => {
       const newHeaders = { ...request.headers, token: "1234" };
       request = { ...request, headers: newHeaders };
-      console.log(request);
-      console.log(process.env.NODE_ENV)
+      console.log(typeof request);
+      console.log(process.env.NODE_ENV);
       return request;
     },
     (error) => {
-      console.log(error);
+      console.log(typeof error);
       return Promise.reject(error);
     }
   );
